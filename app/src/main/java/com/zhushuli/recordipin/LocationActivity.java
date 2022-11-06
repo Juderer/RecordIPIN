462465b8a476614ef23edd7d5d2f092d6796e619
Juderer <zsl.1997@qq.com>
2022-11-06 22:36:12 +0800

    record Location in service

--- app/src/main/java/com/zhushuli/recordipin/LocationActivity.java
+++ app/src/main/java/com/zhushuli/recordipin/LocationActivity.java
@@ -41,20 +41,11 @@ public class LocationActivity extends AppCompatActivity implements ServiceConnec
     private static final int GNSS_LOCATION_UPDATE_CODE = 8402;
     private static final int GNSS_SEARCHING_CODE = 502;
     private static final int GNSS_PROVIDER_DISABLED_CODE = 404;
-    private static final int GNSS_FILE_WRITING_CODE = 1010;
-    private static final int GNSS_FILE_CLOSE_CODE = 2020;
-    private static final int GNSS_DOWNLOAD_THREAD_INTERRUPT_CODE = 3030;
 
     private TextView tvLocationMsg;
     private Button btnLocServiceStart;
 
     private LocationService.MyBinder binder = null;
-
-    private SimpleDateFormat formatter;
-
-    private DownloadThread mDownloadThread;
-    private String mRecordingDir;
-    private ArrayList<String> mLocationStrList = new ArrayList<String>();
 
     private Handler mMainHandler = new Handler(Looper.getMainLooper()) {
         @Override
@@ -88,15 +79,6 @@ public class LocationActivity extends AppCompatActivity implements ServiceConnec
         tvLocationMsg = (TextView) findViewById(R.id.tvLocationMsg);
         btnLocServiceStart = (Button) findViewById(R.id.btnLocServiceStart);
         btnLocServiceStart.setOnClickListener(this);
-
-        formatter = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss");
-
-        // 存储文件路径
-        mRecordingDir = getExternalFilesDir(
-                Environment.getDataDirectory().getAbsolutePath()).getAbsolutePath();
-
-        mDownloadThread = new DownloadThread(mRecordingDir);
-        new Thread(mDownloadThread).start();
     }
 
     @Override
@@ -104,6 +86,7 @@ public class LocationActivity extends AppCompatActivity implements ServiceConnec
         Log.d(TAG, "onServiceConnected");
         binder = (LocationService.MyBinder) service;
         LocationService mLocationService = binder.getLocationService();
+        mLocationService.startLocationRecording();
         mLocationService.setCallback(new LocationService.Callback() {
             @Override
             public void onLocationChanged(Location location) {
@@ -112,9 +95,6 @@ public class LocationActivity extends AppCompatActivity implements ServiceConnec
                 msg.what = GNSS_LOCATION_UPDATE_CODE;
                 msg.obj = LocationStrUtils.printLocationMsg(location);
                 mMainHandler.sendMessage(msg);
-
-                // TODO::手机锁屏后无法保存数据
-                writeLocation2File(location);
             }
 
             @Override
@@ -125,8 +105,6 @@ public class LocationActivity extends AppCompatActivity implements ServiceConnec
 
                 unbindService(LocationActivity.this);
                 binder = null;
-
-                closeLocation2File();
             }
 
             @Override
@@ -159,126 +137,11 @@ public class LocationActivity extends AppCompatActivity implements ServiceConnec
                     binder = null;
                     tvLocationMsg.setText("Location Stop");
 
-                    closeLocation2File();
-
                     btnLocServiceStart.setText("Start");
                 }
                 break;
             default:
                 break;
-        }
-    }
-
-    public void writeLocation2File(Location location) {
-        // 每11秒保存一次
-        if (mLocationStrList.size() > 10) {
-            String csvString = "";
-            for (String locStr : mLocationStrList) {
-                csvString += locStr;
-            }
-            Message msg = Message.obtain();
-            msg.what = GNSS_FILE_WRITING_CODE;
-            msg.obj = csvString;
-            mDownloadThread.getHandler().sendMessage(msg);
-            mLocationStrList.clear();
-            Log.d(TAG, "writing");
-        } else {
-            mLocationStrList.add(LocationStrUtils.genLocationCsv(location));
-        }
-    }
-
-    public void closeLocation2File() {
-        Message msg = Message.obtain();
-        msg.what = GNSS_FILE_CLOSE_CODE;
-        if (mLocationStrList.size() > 0) {
-            // TODO::Java拼接字符串列表
-            String csvString = "";
-            for (String locStr : mLocationStrList) {
-                csvString += locStr;
-            }
-            msg.obj = csvString;
-            mLocationStrList.clear();
-        } else {
-            msg.obj = null;
-        }
-        mDownloadThread.getHandler().sendMessage(msg);
-    }
-
-    public class DownloadThread implements Runnable {
-        private BufferedWriter mBufferWriter;
-        private Handler mHandler;
-        private String mDirRootPath;
-        private AtomicBoolean bFileWriterOpen = new AtomicBoolean(false);
-
-        public DownloadThread(String dirRootPath) {
-            mDirRootPath = dirRootPath;
-        }
-
-        private void initWriter() {
-            String dirPath = mDirRootPath + File.separator + formatter.format(new Date(System.currentTimeMillis()));
-            mBufferWriter = FileUtils.initWriter(dirPath, "GNSS.csv");
-            try {
-                mBufferWriter.write("sysTime,gnssTime,longitude,latitude,accuracy,speed,speedAccuracy,bearing,bearingAccuracy\n");
-            } catch (IOException e) {
-                e.printStackTrace();
-                Log.d(TAG, "1111");
-            }
-        }
-
-        @Override
-        public void run() {
-            Looper.prepare();
-            Log.d(TAG, "Download Thread Start");
-            mHandler = new Handler(Looper.myLooper()) {
-                @Override
-                public void handleMessage(@NonNull Message msg) {
-                    switch (msg.what) {
-                        case GNSS_DOWNLOAD_THREAD_INTERRUPT_CODE:
-                            Looper.myLooper().quit();
-                            break;
-                        case GNSS_FILE_WRITING_CODE:
-                            Log.d(TAG, "Download Thread Write");
-                            if (!bFileWriterOpen.getAndSet(true)) {
-                                initWriter();
-                            }
-                            try {
-                                mBufferWriter.write((String) msg.obj);
-                                mBufferWriter.flush();  // 缓冲区未满时强制数据写入
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                                Log.d(TAG, "2222");
-                            }
-                            break;
-                        case GNSS_FILE_CLOSE_CODE:
-                            Log.d(TAG, "Download Thread Close");
-                            if (!bFileWriterOpen.get()) {
-                                if (msg.obj != null) {
-                                    initWriter();
-                                } else {
-                                    break;
-                                }
-                            }
-                            try {
-                                if (msg.obj != null) {
-                                    mBufferWriter.write((String) msg.obj);
-                                }
-                                mBufferWriter.close();
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
-                            bFileWriterOpen.set(false);
-                            break;
-                        default:
-                            break;
-                    }
-                }
-            };
-            Looper.loop();
-            Log.d(TAG, "Download Thread Interrupt");
-        }
-
-        public Handler getHandler() {
-            return mHandler;
         }
     }
 
@@ -317,12 +180,6 @@ public class LocationActivity extends AppCompatActivity implements ServiceConnec
         Log.d(TAG, "onRestart");
     }
 
-    protected void interruptDownloadThread() {
-        Message msg = Message.obtain();
-        msg.what = GNSS_DOWNLOAD_THREAD_INTERRUPT_CODE;
-        mDownloadThread.getHandler().sendMessage(msg);
-    }
-
     @Override
     protected void onDestroy() {
         super.onDestroy();
@@ -332,8 +189,5 @@ public class LocationActivity extends AppCompatActivity implements ServiceConnec
             unbindService(LocationActivity.this);
             binder = null;
         }
-
-        closeLocation2File();
-        interruptDownloadThread();
     }
 }
